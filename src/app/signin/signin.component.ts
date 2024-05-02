2d980127431fbb996f52796ee6366ef5bc3044f6
BattuRani2002 <19d41a0524@gmail.com>
2024-05-02 12:17:34 +0530

    canva application setup

---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.css'
+})
+export class SigninComponent {
+  showAdditionalContent: boolean = false;
+
+  toggleSections() {
+    this.showAdditionalContent = true;
+
+  }
+  
+
+}
